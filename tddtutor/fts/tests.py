6d4d5e967978c9bd91f49f1df1998322cfcdf731
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-04-16 16:17:36 +0300

    Changed selenium usage to find_element(By.XXX)

--- tddtutor/fts/tests.py
+++ tddtutor/fts/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
@@ -18,22 +19,21 @@ class PollsTest(LiveServerTestCase):
         self.browser.get(self.live_server_url + '/admin/')
 
         # She sees the familiar 'Django administration' heading
-        body = self.browser.find_element_by_tag_name('body')
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Django administration', body.text)
 
         # She types in her username and password and hits return
-        username_field =  self.browser.find_element_by_name('username')
+        username_field =  self.browser.find_element(By.NAME, 'username')
         username_field.send_keys('admin')
 
-        password_field = self.browser.find_element_by_name('password')
+        password_field = self.browser.find_element(By.NAME, 'password')
         password_field.send_keys('pass')
         password_field.send_keys(Keys.RETURN)
 
         # her username and password are accepted, and she is taken to
         # the Site Administration page
-        body = self.browser.find_element_by_tag_name('body')
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Site administration', body.text)
-
 
 
         # todo: use the admin site to create a Poll
