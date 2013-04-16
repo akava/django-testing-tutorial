2a171a649a87d1299fe3175e75c396b2a326a90a
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-04-16 15:41:29 +0300

    project created, setup, first test added

---
+++ tddtutor/fts/tests.py
@@ -0,0 +1,20 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+
+class PollsTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        self.browser.get(self.live_server_url + '/admin/')
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        # todo: use the admin site to create a Poll
+        self.fail('finish this test')
