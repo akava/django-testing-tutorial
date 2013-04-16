f8562f745498dd53fa6e70e3a25d539dfc5f1a45
Andrei Kavaleu <andrei.kavaleu@gmail.com>
2013-04-16 16:08:56 +0300

    Successfully logged into admin site

--- tddtutor/fts/tests.py
+++ tddtutor/fts/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class PollsTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -11,10 +14,27 @@ class PollsTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_can_create_new_poll_via_admin_site(self):
+        # Kate opens her web browser, and goes to the admin page
         self.browser.get(self.live_server_url + '/admin/')
 
+        # She sees the familiar 'Django administration' heading
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
+
+        # She types in her username and password and hits return
+        username_field =  self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('pass')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+
 
         # todo: use the admin site to create a Poll
         self.fail('finish this test')
